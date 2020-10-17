d48be3b23d85f168fc793fab5e344410a36eabc3
Ondřej Suchomel <ondrej_suchomel@centrum.cz>
2020-10-17 20:37:16 +0200

    Added more stuff to Terraform, updated ReadMe, added script for upload to s3 - all needs testing

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,6 +2,7 @@ import sched
 import time
 import getWeatherInformation
 import createJsonFile
+import s3upload
 s = sched.scheduler(time.time, time.sleep)
 
 def run_hourly_download(sc): 
@@ -10,11 +11,18 @@ def run_hourly_download(sc):
     apiKey = '44419df6dc22898347ae3db58aa344d5'
     units = 'metric'
     url = 'https://api.openweathermap.org/data/2.5/weather'
+    # json with data for regular uploads
+    pragueWeatherData = 'data/pragueWeatherData.json'
+    #bucket and file information
+    bucket = 'MSD HW S3 Bucket'
+    s3FileName = 'pragueWeatherData.json'
 
     print("Running download...")
     getWeatherInformation.downloadWeatherInformation(url, id, apiKey, units)
     createJsonFile.createJsonFile()
-    s.enter(3600, 1, run_hourly_download, (sc,))
+    s3upload.upload_to_aws(pragueWeatherData, bucket, s3FileName)
+    s.enter(36, 1, run_hourly_download, (sc,)) #TODO change to 3600
 
-s.enter(3600, 1, run_hourly_download, (s,))
+s3upload.upload_to_aws('index.html', 'MSD HW S3 Bucket', 'index.html')
+s.enter(36, 1, run_hourly_download, (s,)) #TODO change to 3600
 s.run()
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,19 +6,46 @@ provider "aws" {
 }
 
 variable "aws_access_key" {
-    type = string
+  type = string
 }
 
 variable "aws_secret_key" {
-    type = string
+  type = string
 }
 
 resource "aws_instance" "EC2_Instance" {
   ami           = "ami-0474863011a7d1541"
   instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_write_profile.id
   tags = {
-    Name = "MSD HW EC2"
+    Name = "MSD HW"
   }
 }
 
+resource "aws_s3_bucket" "S3_Bucket" {
+  bucket = "MSD HW S3 Bucket"
+  acl    = "public-read"
+  tags = {
+    Name = "MSD HW"
+    # Environment = "Dev"
+  }
+}
 
+resource "aws_iam_role" "ec2_s3_write_role" {
+  name = "ec2_s3_write_role"
+  assume_role_policy = file("ec2-assume-policy.json")
+  tags = {
+    Name = "MSD HW"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_s3_write_policy" {
+  name = "ec2_s3_write_policy"
+  role = aws_iam_role.ec2_s3_write_role.id
+  policy = file("ec2-policy.json")
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_write_profile" {
+  name = "ec2_s3_write_profile"
+  role = aws_iam_role.ec2_s3_write_role.id
+}
