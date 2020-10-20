279fcd2e7ccba0f6e99ca221154d42fcf78f1e28
Ondřej Suchomel <ondrej_suchomel@centrum.cz>
2020-10-20 19:27:48 +0200

    Updates to docker, app and terraform set up.

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_instance" "ec2_instance" {
   sudo apt install awscli -y
   sudo curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
-  sudo docker run -e AWS_S3_BUCKET="${var.bucket_name}" -e APP_TIMER="${var.app_timer}" -e OWM_API_KEY="${var.owm_api_key}" OWM_CITY_ID="${var.owm_city_id}"  ondrejsuchomel/msd-hw-dockerized-app
+  sudo docker run -e AWS_S3_BUCKET="${var.bucket_name}" -e APP_TIMER="${var.app_timer}" -e OWM_API_KEY="${var.owm_api_key}" -e OWM_CITY_ID="${var.owm_city_id}"  ondrejsuchomel/msd-hw-dockerized-app
   EOF
 
 
